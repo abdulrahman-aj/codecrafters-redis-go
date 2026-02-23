5302bb046a6db52d1cd8009e16708d6eced0cfd9
abdulrahman-aj <ajlouni2000@gmail.com>
2026-02-23 09:00:45 +0300

    codecrafters submit [skip ci]

--- app/server/engine/commands/psync.go
+++ app/server/engine/commands/psync.go
@@ -1,12 +1,21 @@
 package commands
 
 import (
+	"encoding/base64"
 	"fmt"
 
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 	"github.com/codecrafters-io/redis-starter-go/app/server/engine/store"
 	"github.com/codecrafters-io/redis-starter-go/app/server/engine/types"
+	"github.com/codecrafters-io/redis-starter-go/app/util"
 )
+
+var emptyRDB = func() []byte {
+	rdbBase64 := `UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==`
+	res, err := base64.StdEncoding.DecodeString(rdbBase64)
+	util.FatalOnErr(err)
+	return res
+}()
 
 type psync struct{}
 
@@ -15,6 +24,7 @@ func parsePsync(command string, args []string) (*psync, []byte) {
 }
 
 func (cmd *psync) Exec(ctx *types.RequestCtx, s *store.Store) ([]byte, error) {
-	msg := fmt.Sprintf("FULLRESYNC %s %d", ctx.Info.MasterReplicationID, ctx.Info.MasterReplicationOffset)
-	return resp.SimpleString(msg), nil
+	msg := resp.SimpleString(fmt.Sprintf("FULLRESYNC %s %d", ctx.Info.MasterReplicationID, ctx.Info.MasterReplicationOffset))
+	msg = fmt.Appendf(msg, "$%d\r\n%s", len(emptyRDB), emptyRDB)
+	return msg, nil
 }
